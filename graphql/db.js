1f49c2d9dc6cc72ec4d0f383fd441b5830b48cff
Hojun Kim <76956067+hojunkim1@users.noreply.github.com>
2022-02-07 23:22:45 +0900

    use graphql with REST api

--- graphql/db.js
+++ graphql/db.js
@@ -1,67 +1,13 @@
-let movies = [
-  {
-    id: 1,
-    name: "Black panther",
-    score: 9.5,
-  },
-  {
-    id: 2,
-    name: "Iron man",
-    score: 7.3,
-  },
-  {
-    id: 3,
-    name: "Shangcii",
-    score: 9,
-  },
-  {
-    id: 4,
-    name: "Eternals",
-    score: 8.8,
-  },
-  {
-    id: 5,
-    name: "Avengers",
-    score: 9.3,
-  },
-  {
-    id: 6,
-    name: "Spiderman",
-    score: 9.1,
-  },
-];
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.am/api/v2/list_movies.json";
 
-export const addMovie = (name, score) => {
-  const id = movies.length + 1;
-  const newMovie = { id, name, score };
-  movies.push(newMovie);
-  return newMovie;
-};
-
-export const getMovie = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  return filteredMovies[0];
-};
-
-export const updateMovie = (id, name, score) => {
-  const movieIndex = movies.findIndex((movie) => movie.id === id);
-  if (movieIndex === -1) {
-    return null;
-  }
-  const movie = movies[movieIndex];
-  const updatedMovie = { ...movie, name, score };
-  movies[movieIndex] = updatedMovie;
-  return updatedMovie;
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (cleanedMovies.length < movies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
+export const getMovies = async (limit = 50, rating = 7) => {
+  const json = await (
+    await fetch(`${API_URL}?limit=${limit}&minimum_rating=${rating}`)
+  ).json();
+  const {
+    data: { movies },
+  } = json;
+  return movies;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,14 +1,8 @@
-import { addMovie, deleteMovie, getById, getMovies, updateMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    getMovie: (_, { id }) => getById(id),
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    updateMovie: (_, { id, name, score }) => updateMovie(id, name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
   },
 };
 
